e1e7203edbf439013cd8211a705b92ac1c666375
Valérian Galliat <val@codejam.info>
2019-02-05 20:50:46 -0500

    Render

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Emoji
+if (document.title.codePointAt(0) !== document.title.charCodeAt(0)) {
+  const emoji = String.fromCodePoint(document.title.codePointAt(0))
+  const canvas = document.createElement('canvas')
+
+  canvas.height = 64
+  canvas.width = 64
+
+  const ctx = canvas.getContext('2d')
+  ctx.font = '64px serif';
+  ctx.fillText(emoji, 0, 64)
+
+  const favicon = document.createElement('link')
+  favicon.rel = 'icon'
+  favicon.href = canvas.toDataURL()
+
+  document.head.appendChild(favicon)
+  document.title = document.title.substr(emoji.length + 1).trim()
+}
